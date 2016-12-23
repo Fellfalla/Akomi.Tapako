2fd6f168fe9b8e9942547c2920c3d42957b050eb
Markus Weber <joop03@alice-dsl.net>
2016-12-23 18:30:07 +0100

    - Changed assembly info for Tapako.Framework - added .nuspec file for Tapako.Framework

--- 03_Realisierung/Tapako.Framework/Properties/AssemblyInfo.cs
+++ 03_Realisierung/Tapako.Framework/Properties/AssemblyInfo.cs
@@ -5,11 +5,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Tapako.Framework")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("This package is needed to write drivers compatible with TAPAKO." +
+                               "Therefore derive from IDeviceCompletement.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+//[assembly: AssemblyCompany("Markus Weber")]
 [assembly: AssemblyProduct("Tapako.Framework")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+//[assembly: AssemblyCopyright("Copyright © AKOMI 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+//[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
