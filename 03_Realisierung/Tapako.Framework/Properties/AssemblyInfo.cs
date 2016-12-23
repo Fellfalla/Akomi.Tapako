a28d0a5eb702aacaeb5ad6d05b6de23eef81c385
Markus Weber <joop03@alice-dsl.net>
2016-12-23 23:18:37 +0100

    removed shared assembly info attributes

--- 03_Realisierung/Tapako.Framework/Properties/AssemblyInfo.cs
+++ 03_Realisierung/Tapako.Framework/Properties/AssemblyInfo.cs
@@ -13,6 +13,8 @@ using System.Runtime.InteropServices;
 //[assembly: AssemblyCopyright("Copyright © AKOMI 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: AssemblyCompany("Markus Weber")]
+[assembly: AssemblyCopyright("Copyright © AKOMI 2016")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
