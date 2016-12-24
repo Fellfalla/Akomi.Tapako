64eb079067b80f0b5e3f71d947d0d8bdd7fd83d1
Markus Weber <joop03@alice-dsl.net>
2016-12-24 14:05:45 +0100

    Added Description content in AssemblyInfo

--- 03_Realisierung/TapakoSolutionInfo.cs
+++ 03_Realisierung/TapakoSolutionInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
 using System.Reflection;
 
-//[assembly: AssemblyCompany("Markus Weber")]
-//[assembly: AssemblyCopyright("Copyright © AKOMI 2016")]
+[assembly: AssemblyCompany("Markus Weber")]
+[assembly: AssemblyCopyright("Copyright © AKOMI 2016")]
