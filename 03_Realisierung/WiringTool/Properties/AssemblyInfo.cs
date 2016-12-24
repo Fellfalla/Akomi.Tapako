1e807b6b8154065d5ae795f17cc6071416c278cb
Markus Weber <joop03@alice-dsl.net>
2016-12-24 14:10:10 +0100

    - Resolved duplicate Assembly Info issues - Added some Assembly Descriptions

--- 03_Realisierung/WiringTool/Properties/AssemblyInfo.cs
+++ 03_Realisierung/WiringTool/Properties/AssemblyInfo.cs
@@ -5,15 +5,15 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Tapako.Utilities.WiringTool")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The Wiring Tool can be used for manually adding existing connections between AKOMI-Devices")]
 [assembly: AssemblyConfiguration("")]
 //[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Tapako.Utilities.WiringTool")]
 //[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyCompany("Markus Weber")]
-[assembly: AssemblyCopyright("Copyright © AKOMI 2016")]
+//[assembly: AssemblyCompany("Markus Weber")]
+//[assembly: AssemblyCopyright("Copyright © AKOMI 2016")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
