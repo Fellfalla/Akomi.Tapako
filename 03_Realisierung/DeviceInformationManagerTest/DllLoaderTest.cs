ae365f7440e43f2157a26ee693de5cca32c6957d
Markus Weber <joop03@alice-dsl.net>
2017-04-15 17:43:39 +0200

    added new test for DllLoader

--- 03_Realisierung/DeviceInformationManagerTest/DllLoaderTest.cs
+++ 03_Realisierung/DeviceInformationManagerTest/DllLoaderTest.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Akomi.InformationModel.Device;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tapako.DeviceInformationManagement.InformationSources;
 using Tapako.Framework;
+using System.Reflection;
+using Akomi.InformationModel.Component.Identification;
 
 namespace DeviceInformationManagerTests
 {
@@ -18,6 +23,37 @@ namespace DeviceInformationManagerTests
         public void LoadDriverTakesNull()
         {
             DllLoader.Load<object>(null);
+        }
+
+
+        [TestMethod]
+        //[Timeout(1000)]
+        public void LoadTestDll()
+        {
+            string filePath = Assembly.GetExecutingAssembly().Location;
+            //string filePath = "DeploymentData/DeviceCompletement.dll";
+            var factory = DllLoader.Load<IDeviceCompletement>(filePath);
+
+            IDevice testClass = new DeviceBase();
+            testClass.Identification = new Identification();
+
+            factory.CompleteDeviceDriver(ref testClass);
+
+            Assert.AreEqual("Dummy1", testClass.Identification.SerialNumber);
+        }
+    }
+
+    public abstract class DummyCompletementBase : IDeviceCompletement
+    {
+        public abstract IDevice CompleteDeviceDriver(ref IDevice deviceRoot);
+    }
+
+    public class DummyCompletement : DummyCompletementBase
+    {
+        public override IDevice CompleteDeviceDriver(ref IDevice deviceRoot)
+        {
+            deviceRoot.Identification.SerialNumber = "Dummy1";
+            return deviceRoot;
         }
     }
 }
